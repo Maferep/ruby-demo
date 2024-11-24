f998ebe01912551176c2f3edd80ac44af9c2bf45
Maferep <mf.pont.tv@gmail.com>
2024-11-24 09:39:53 -0300

    bad test

--- app.rb
+++ app.rb
@@ -3,7 +3,20 @@ require "cuba"
 ITEMS = ("A".."Z").to_a
 
 Cuba.define do
-  on default do
-    res.write "Hello world"
+  on get do
+    on root do
+      res.write "Hello world"
+    end
+  end
+
+  on post do
+    on "product" do
+      on param("id"), param("name") do |id, name|
+        res.write ""
+      end
+      on true do
+        res.write("Need id and name parameters")
+      end
+    end
   end
 end
--- test.rb
+++ test.rb
@@ -10,3 +10,10 @@ scope do
     assert_equal "Hello world", last_response.body
   end
 end
+
+scope do
+  test "Submit" do
+    post("/product", { "id" => 1, "name" => "pizza" }, 'CONTENT_TYPE' => 'query_params')
+    assert_equal "Need id and name parameters", last_response.body
+  end
+end
